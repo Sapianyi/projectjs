e65f174af8e573e1ad1643838915520a3d5f3a3c
Mikhailo Sapianyi <sapianyi@gmail.com>
2024-07-31 10:20:23 +0300

    017

---
+++ js/script017.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const str = "test";
+const arr = [2, 4, 6];
+
+console.log(str.toUpperCase());
+console.log(str);
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf('fruit'));
+
+const logg = "Hello world";
+console.log(logg.slice(6, 11));
+console.log(logg.substring(6, 11));
+console.log(logg.substr(6, 2));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.6px";
+console.log(parseInt(test));
